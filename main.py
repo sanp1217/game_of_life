7281fc7a641d378e94936803846b40f3e4a310ab
Pablo <pablosgrad12@gmail.com>
2022-12-26 18:09:48 -0700

    Adds dead_state function which initializes cells to 0

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import random
 
 
+def dead_state(width, height):
+    state = [[0 for i in range(width)] for j in range(height)]
+    return state
 
+def main():
+    print((dead_state(2, 5)))
+
+if __name__ == "__main__":
+    main()
