ca965f5f4dc337834fb2ee210ff3d3bdbc710460
Pablo <pablosgrad12@gmail.com>
2022-12-28 12:18:12 -0700

    Takes off semicolon and uses different name for variable in second for loop.

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import random
 
+
 def random_state(width, height):
     state = []
 
     for i in range(height):
         temp = []
-        for i in range(width):
-            random_num = random.random();
+        for j in range(width):
+            random_num = random.random()
             if random_num >= 0.5:
                 cell_state = 0
             else:
@@ -18,7 +19,8 @@ def random_state(width, height):
 
 
 def main():
-    print((random_state(2, 5)))
+    print((random_state(1, 4)))
+
 
 if __name__ == "__main__":
     main()
