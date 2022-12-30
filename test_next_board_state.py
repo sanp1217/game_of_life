5e160c0e0cb0be414c51d2d94049d73fbb0aa124
Pablo <pablosgrad12@gmail.com>
2022-12-29 17:24:38 -0700

    Adds test_next_board function to remove repeated code

--- test_next_board_state.py
+++ test_next_board_state.py
@@ -1,6 +1,20 @@
 from main import next_board_state
 
 
+def test_next_board(init_state, expected_next_state, test_num):
+    actual_next_state = next_board_state(init_state)
+
+    if expected_next_state == actual_next_state:
+        print("PASSED " + str(test_num))
+    else:
+        print("FAILED " + str(test_num))
+        print("Expected:")
+        print(expected_next_state)
+        print("Actual:")
+        print(actual_next_state)
+
+    print()
+
 if __name__ == "__main__":
     # TEST 1: dead cells with no live neighbors
     # should stay dead.
@@ -14,16 +28,8 @@ if __name__ == "__main__":
         [0,0,0],
         [0,0,0]
     ]
-    actual_next_state1 = next_board_state(init_state1)
 
-    if expected_next_state1 == actual_next_state1:
-        print("PASSED 1")
-    else:
-        print("FAILED 1!")
-        print("Expected:")
-        print(expected_next_state1)
-        print("Actual:")
-        print(actual_next_state1)
+    test_next_board(init_state1, expected_next_state1, 1)
 
     # TEST 2: dead cells with exactly 3 neighbors
     # should come alive.
@@ -37,13 +43,5 @@ if __name__ == "__main__":
         [0,1,1],
         [0,0,0]
     ]
-    actual_next_state2 = next_board_state(init_state2)
 
-    if expected_next_state2 == actual_next_state2:
-        print("PASSED 2")
-    else:
-        print ("FAILED 2!")
-        print ("Expected:")
-        print (expected_next_state2)
-        print ("Actual:")
-        print (actual_next_state2)
+    test_next_board(init_state2, expected_next_state2, 2)
